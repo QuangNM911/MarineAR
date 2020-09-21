677c26867855b25efa2907604d27dfc454a6571d
QNM911 <quangnmptit@gmail.com>
2020-09-22 00:24:52 +0700

    add arfoundation package

---
+++ Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class PlaceObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToPlace;
+    
+    List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
+
+    [SerializeField]
+    ARRaycastManager aRRaycastManager;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0){
+            Touch t = Input.GetTouch(0);
+            if(t.phase == TouchPhase.Began){
+                if(aRRaycastManager.Raycast(t.position, raycastHits, TrackableType.PlaneWithinPolygon)){
+                    Pose hitPose = raycastHits[0].pose;
+                    Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
